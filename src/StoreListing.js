9d9ee545db490bdbbe8425ea4ba0d670a507eddc
Emily Ochoa <eochoa156@gmail.com>
2024-04-21 12:07:11 -0700

    store listing and top bar files made

---
+++ src/StoreListing.js
@@ -0,0 +1,34 @@
+
+<Container style={{ marginTop: "40px", textAlign: "center" }}>
+    <Row style={{ borderBottom: "1px solid solid #E8E9EB", display: "flex", justifyContent: "space-evenly" }}>
+        <Row>
+            <Col xs=".5">
+                <Image src="{IMG SRC}" roundedCircle/>
+            </Col>
+            <Col>
+            <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
+            <p>Delivery * Pickup</p>
+            </Col>
+        </Row>
+        <Col sm="1" />
+        <Row>
+            <Col xs=".5">
+                <Image src="{IMG SRC}" roundedCircle/>
+            </Col>
+            <Col>
+            <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
+            <p>Delivery * Pickup</p>
+            </Col>
+        </Row>
+        <Col sm="1" />
+        <Row>
+            <Col xs=".5">
+                <Image src="{IMG SRC}" roundedCircle/>
+            </Col>
+            <Col>
+            <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
+            <p>Delivery * Pickup</p>
+            </Col>
+        </Row>
+    </Row>
+</Container>
